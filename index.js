2f93de8d053ebe6cd288acd365b72d1f3c171448
Noelle <noelle.maingi@student.moringaschool.com>
2023-07-07 01:05:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word.length < 2) {
+    return true;
+  }
+
+  let first = 0;
+  let last = word.length - 1;
+
+  while (first <= last) {
+    if (word[first] != word[last]) {
+      return false;
+    }
+    first++;
+    last--;
+  }
+  return true;
 }
 
 /* 
